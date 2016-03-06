4208131941758dbd677118c2e043a3ba214e62d1
Nick Mckloski <Mckloski.Nick@gmail.com>
2016-03-06 17:32:34 -0500

    project changes

--- js/app/controllers/projects.js
+++ js/app/controllers/projects.js
@@ -1,44 +1,63 @@
 app.controller("projects", ['$scope', function($scope) {
 	//traffic
+	$scope.trafficThumbnailUrl = "img/previews/thumbnails/traffictools";
 	$scope.trafficBaseUrl = "img/previews/traffictools";
 	$scope.trafficCount = 10;
+	$scope.trafficThumbnails = [];
 	$scope.trafficPics = [];
+
 	for(i = 1; i <= $scope.trafficCount; i++) {
-		$scope.trafficPics.push($scope.trafficBaseUrl+i+".png")
+		$scope.trafficThumbnails.push($scope.trafficThumbnailUrl+i+".png");
+		$scope.trafficPics.push($scope.trafficBaseUrl+i+".png");
 	}
 	//gamestat
+	$scope.gameThumbnailUrl = "img/previews/thumbnails/gamestat";
 	$scope.gameBaseUrl = "img/previews/gamestat";
 	$scope.gameStatCount = 3;
+	$scope.gameStatThumbnails = [];
 	$scope.gameStatPics = [];
 	for(i = 1; i <= $scope.gameStatCount; i++) {
-		$scope.gameStatPics.push($scope.gameBaseUrl+i+".png")
+		$scope.gameStatThumbnails.push($scope.gameThumbnailUrl+i+".png");
+		$scope.gameStatPics.push($scope.gameBaseUrl+i+".png");
 	}
 	//rss
+	$scope.rssThumbnailUrl = "img/previews/thumbnails/rss";
 	$scope.rssBaseUrl = "img/previews/rss";
 	$scope.rssCount = 3;
+	$scope.rssThumbnails = [];
 	$scope.rssPics = [];
 	for(i = 1; i <= $scope.rssCount; i++) {
-		$scope.rssPics.push($scope.rssBaseUrl+i+".png")
+		$scope.rssThumbnails.push($scope.rssThumbnailUrl+i+".png");
+		$scope.rssPics.push($scope.rssBaseUrl+i+".png");
 	}
 	//budget
+	$scope.budgetThumbnailUrl = "img/previews/thumbnails/budget";
 	$scope.budgetBaseUrl = "img/previews/budget";
 	$scope.budgetCount = 3;
+	$scope.budgetThumbnails = [];
 	$scope.budgetPics = [];
 	for(i = 1; i <= $scope.budgetCount; i++) {
-		$scope.budgetPics.push($scope.budgetBaseUrl+i+".png")
+		$scope.budgetThumbnails.push($scope.budgetThumbnailUrl+i+".png");
+		$scope.budgetPics.push($scope.budgetBaseUrl+i+".png");
 	}
 	//bank app
+	$scope.bankThumbnailUrl = "img/previews/thumbnails/bank";
 	$scope.bankBaseUrl = "img/previews/bank";
 	$scope.bankCount = 1;
+	$scope.bankThumbnails = [];
 	$scope.bankPics = [];
 	for(i = 1; i <= $scope.bankCount; i++) {
-		$scope.bankPics.push($scope.bankBaseUrl+i+".png")
+		$scope.bankThumbnails.push($scope.bankThumbnailUrl+i+".png");
+		$scope.bankPics.push($scope.bankBaseUrl+i+".png");
 	}
 	//personal web
+	$scope.personalWebThumbnailsUrl = "img/previews/thumbnails/web";
 	$scope.personalWebBaseUrl = "img/previews/web";
 	$scope.personalWebCount = 1;
+	$scope.personalWebThumbnails = [];
 	$scope.personalWebPics = [];
 	for(i = 1; i <= $scope.personalWebCount; i++) {
-		$scope.personalWebPics.push($scope.personalWebBaseUrl+i+".png")
+		$scope.personalWebThumbnails.push($scope.personalWebThumbnailsUrl+i+".png");
+		$scope.personalWebPics.push($scope.personalWebBaseUrl+i+".png");
 	}
 }]);
